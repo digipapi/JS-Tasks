bc39dcbdd71e04d00d3aecc7c53c61c2bc19a3a6
Joshua Falonipe <joshfalonipe@gmail.com>
2020-03-23 21:04:03 +0100

    first commit

---
+++ js1.js
@@ -0,0 +1,10 @@
+var my = "Joshua Falonipe";
+console.log(my);
+var courses = ["HTML", "CSS", "JAVASCRIPT", "UI/UX"];
+console.log(courses);
+var i = i;
+for (i = 1; i <= 200; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}
